9c55d4aa4bec89cce2db909a7a600be61a50db83
Imran9001 <122745336+Imran9001@users.noreply.github.com>
2025-06-26 17:37:40 +0100

    Update Operations.js

--- To Do List React Front End/ToDoList/src/Operations.js
+++ To Do List React Front End/ToDoList/src/Operations.js
@@ -1,30 +1,24 @@
-const axios = require('axios');
+import axios from 'axios';
 
 const API_BACKEND_URL = "https://api.todolist.it.com/todo";
 
- const fetchTasksAPI = async () => {
+export const fetchTasksAPI = async () => {
     const response = await axios.get(API_BACKEND_URL);
     return response.data;  
 };
 
- const addTaskAPI = async (title) => {
+export const addTaskAPI = async (title) => {
     const response = await axios.post(API_BACKEND_URL, {title});
     return response.data;
 };
 
- const updateTaskAPI = async(id, update) => {
+export const updateTaskAPI = async (id, update) => {
     const response = await axios.put(`${API_BACKEND_URL}/${id}`, update);
     return response.data;
-}
+};
 
- const deleteTaskAPI = async(id) => {
+export const deleteTaskAPI = async (id) => {
     const response = await axios.delete(`${API_BACKEND_URL}/${id}`);
     return response.data;
 };
 
-module.exports = {
-    fetchTasksAPI,
-    addTaskAPI,
-    updateTaskAPI,
-    deleteTaskAPI
-};
