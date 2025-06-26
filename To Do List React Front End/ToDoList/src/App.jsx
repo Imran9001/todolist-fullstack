e4d8036f6065df2f01f955f34e730c0063d061ac
Imran9001 <122745336+Imran9001@users.noreply.github.com>
2025-06-26 17:48:16 +0100

    Update App.jsx
    Testing AWS deployment

--- To Do List React Front End/ToDoList/src/App.jsx
+++ To Do List React Front End/ToDoList/src/App.jsx
@@ -72,7 +72,7 @@ function App() {
 
   return (
     <div style = {{padding: '20px', fontFamily: 'Arial, sans-serif'}}>
-      <h1>To Do List</h1>
+      <h1>To Do List Test AWS</h1>
       <TaskForm onAddTask ={addTask}/>
       <TaskList
       tasks = {tasks}
